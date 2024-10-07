b45c023c191dfc2f4b3eb7fe91fb6137ef726af4
Rahul Rasve <rahulrasve712@gmail.com>
2024-10-07 16:53:30 +0530

    signup form done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { LoginForm } from './Components/LoginForm/LoginForm';
+import { SignUpForm } from './Components/SignUpForm/SignUpForm';
 
 function App() {
   return (
     <div>
-      <LoginForm />
+      <SignUpForm />
     </div>
   );
 }
